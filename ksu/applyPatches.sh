2191b669994c3813324fae770a6f0f419e70661b
Edmond2007 <edmondseropyan6@gmail.com>
2025-06-19 16:51:30 +0000

    modified:   ksu/applyPatches.sh

--- ksu/applyPatches.sh
+++ ksu/applyPatches.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
-set -e
 
 export maindir="$(pwd)"
 export outside="${maindir}/.."
 source "${outside}/env"
 
-cd KernelSU
-./build.sh kernel_patch "${outside}"
+KSU_git_ver=$(cd KernelSU && git rev-list --count HEAD)
+KSU_ver=$(($KSU_git_ver + 10000 + 200))
 
-# Версия KSU для баннера
-KSU_git_ver=$(git rev-list --count HEAD)
-KSU_ver=$((KSU_git_ver + 10000 + 200))
+patchesdir="$outside/ksu/patches/4.19"
+for patch_file in "$patchesdir"/*.patch ; do
+  patch -p1 < "$patch_file"
+done
 
-cd "${maindir}"
+sed -i "s/\(CONFIG_LOCALVERSION=\)\(.*\)/\1\"-${kernel_name}-KSU${KSU_ver}\"/" "${defconfig_file}"
 
-# Обновляем CONFIG_LOCALVERSION
-sed -i "s/CONFIG_LOCALVERSION=.*/\1\"-${kernel_name}-KSU${KSU_ver}\"/" "${defconfig_file}"
 echo "$(grep 'CONFIG_LOCALVERSION=' ${defconfig_file})"
 
 echo "includes KernelSU ${KSU_ver}" >> banner_append
+
