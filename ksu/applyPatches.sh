c523465038150a76b6947ec46c422441f968b721
Edmond2007 <edmondseropyan6@gmail.com>
2025-06-20 16:03:49 +0000

    testing another defconfig

--- ksu/applyPatches.sh
+++ ksu/applyPatches.sh
@@ -7,6 +7,8 @@ source "${outside}/env"
 KSU_git_ver=$(cd KernelSU && git rev-list --count HEAD)
 KSU_ver=$(($KSU_git_ver + 10000 + 200))
 
+echo "$outside"
+
 patchesdir="$outside/ksu/patches/4.19"
 for patch_file in "$patchesdir"/*.patch ; do
   patch -p1 < "$patch_file"
