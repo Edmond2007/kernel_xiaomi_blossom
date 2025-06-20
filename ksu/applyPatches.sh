0fe7d3200f8b182093be6d4dd6d0da2ca8b17ef8
Edmond2007 <edmondseropyan6@gmail.com>
2025-06-20 17:06:01 +0000

    disabled modules

--- ksu/applyPatches.sh
+++ ksu/applyPatches.sh
@@ -7,10 +7,11 @@ source "${outside}/env"
 KSU_git_ver=$(cd KernelSU && git rev-list --count HEAD)
 KSU_ver=$(($KSU_git_ver + 10000 + 200))
 
-patchesdir="/home/runner/work/kernel_xiaomi_blossom/ksu/patches/4.19"
-for patch_file in "$patchesdir"/*.patch ; do
-  patch -p1 < "$patch_file"
-done
+patch -p1 < patches/4.19/0001-fs-exec-add-ksu-execveat-hook.patch
+patch -p1 < patches/4.19/0002-fs-open-add-ksu-faccessat-hook.patch
+patch -p1 < patches/4.19/0003-fs-read_write-add-ksu-vfs_read-hook.patch
+patch -p1 < patches/4.19/0004-fs-stat-add-ksu-vfs_statx-hook.patch
+patch -p1 < patches/4.19/0005-drivers-input-input-add-ksu-input_handle_event-hook.patch
 
 sed -i "s/\(CONFIG_LOCALVERSION=\)\(.*\)/\1\"-${kernel_name}-KSU${KSU_ver}\"/" "${defconfig_file}"
 
