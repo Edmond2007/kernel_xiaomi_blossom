557e5a4758d5049b9f7a86274fa23666a815969c
Edmond2007 <edmondseropyan6@gmail.com>
2025-06-23 08:42:40 +0000

    path_umount to -ENOSYS;

--- fs/susfs.c
+++ fs/susfs.c
@@ -1162,7 +1162,7 @@ int susfs_sus_memfd(char *memfd_name) {
 }
 
 static void umount_mnt(struct path *path, int flags) {
-	int err = path_umount(path, flags);
+	int err = -ENOSYS;
 	if (err) {
 		SUSFS_LOGI("umount %s failed: %d\n", path->dentry->d_iname, err);
 	}
